2871fe0aa12642ff1e3f80dda1bed13a597cb587
Thorsten Behrens <tbehrens@novell.com>
2011-09-08 14:06:42 +0200

    Set dates and info for the 2011 Board Election

--- vote/2011/mkical.py
+++ vote/2011/mkical.py
@@ -14,15 +14,13 @@ import vobject
 
 #### Configure these variables
 YEAR = 2011
-CANDIDATES_OPENED_DATE    = (YEAR, 5,  9) # In 2012, begin earlier in May
-APPLICATIONS_CLOSED_DATE  = (YEAR, 5, 13) # We do not do that anymore, as we cannot guarantee an application to be processed anyway, if contacts are not responding
-RENEWALS_CLOSED_DATE      = (YEAR, 5, 20) # Not used, renewals are incorporated into applications
-CANDIDATES_CLOSED_DATE    = (YEAR, 5, 22)
-CANDIDATES_ANNOUNCED_DATE = (YEAR, 5, 25)
-VOTING_OPENED_DATE        = (YEAR, 5, 29)
-VOTING_CLOSED_DATE        = (YEAR, 6, 12)
-PRELIMINARY_RESULTS_DATE  = (YEAR, 6, 14)
-CHALLENGE_CLOSED_DATE     = (YEAR, 6, 21)
+CANDIDATES_OPENED_DATE    = (YEAR, 9,   2) # In 2012, begin earlier in May
+CANDIDATES_CLOSED_DATE    = (YEAR, 10,  1)
+CANDIDATES_ANNOUNCED_DATE = (YEAR, 10,  3)
+VOTING_OPENED_DATE        = (YEAR, 10,  7)
+VOTING_CLOSED_DATE        = (YEAR, 10, 17)
+PRELIMINARY_RESULTS_DATE  = (YEAR, 10, 20)
+CHALLENGE_CLOSED_DATE     = (YEAR, 10, 26)
 
 
 
@@ -50,32 +48,16 @@ def d(year, month, day):
 CANDIDATES_OPENED = (
     d(*CANDIDATES_OPENED_DATE),
     'Announcements and list of candidates opens',
-    c("""If you are a member of the GNOME Foundation and are interested 
+    c("""If you are a member of The Document Foundation and are interested 
     in running for election, you may nominate yourself by sending an 
-    e-mail to foundation-announce@gnome.org with your name, e-mail 
+    e-mail to elections@documentfoundation.org with your name, e-mail 
     address, corporate affiliation (if any), and a description of why 
     you'd like to serve, before
     %s (23:59 UTC).""" % d(*CANDIDATES_CLOSED_DATE)) + '''
     ''' + c("""    
     You should also send a summary of your candidacy announcement 
-    (75 words or less) to elections@gnome.org. If you are not yet a 
-    GNOME Foundation member and would like to stand for election, 
-    you must first apply for membership and be accepted to be eligible 
-    to run. (You may, however, announce your candidacy prior to formal 
-    acceptance of your application;
-    should your application not be accepted, you will not be included in 
-    the list of candidates.)""") + '''
+    (75 words or less) to discuss@documentfoundation.org. """) + '''
     '''
-)
-
-APPLICATIONS_CLOSED = (
-    d(*APPLICATIONS_CLOSED_DATE),
-    'Applications/Renewals closed',
-    c("""If you're not a member of the GNOME Foundation or if 
-    you need to renew your membership, you have to apply at
-    http://foundation.gnome.org/membership/application.php before
-    %s (23:59 UTC).
-    """ % d(*APPLICATIONS_CLOSED_DATE))
 )
 
 CANDIDATES_CLOSED = (
@@ -88,15 +70,6 @@ CANDIDATES_ANNOUNCED = (
     d(*CANDIDATES_ANNOUNCED_DATE),
     'List of candidates announced',
     'You may now start to send your questions to the candidates'
-)
-
-RENEWALS_CLOSED = (
-    d(*RENEWALS_CLOSED_DATE),
-    'Renewals Closed',
-    c("""If you are a GNOME Foundation member and your membership status
-    runs out before %s, you must apply for renewal of your membership status
-    before %s.""" % (d(*VOTING_OPENED_DATE), d(*RENEWALS_CLOSED_DATE))
-    )
 )
 
 VOTING_OPENED = (
@@ -261,10 +234,8 @@ if __name__ == "__main__":
     
     eventlist = [
         CANDIDATES_OPENED,
-        #APPLICATIONS_CLOSED,
         CANDIDATES_CLOSED,
         CANDIDATES_ANNOUNCED,
-        #RENEWALS_CLOSED,
         VOTING_OPENED,
         VOTING_CLOSED,
         PRELIMINARY_RESULTS,
